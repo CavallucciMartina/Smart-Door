c9938287d6d0169b93cc6eea908be1005b4cbbab
MATTEO MAGNINI <matteo.magnini@studio.unibo.it>
2018-02-13 10:06:54 +0100

    arduino, fritzing and statechart compleated

---
+++ smart_door/InformationTask.cpp
@@ -0,0 +1,26 @@
+#include "InformationTask.h"
+#include "Config.h"
+
+#define MSG_INFORMATION "I:"
+
+InformationTask::InformationTask(Door* pDoor, TempSensor* temp, LedExt* ledValue){
+  this->pDoor = pDoor;
+  this->temp = temp;
+  this->ledValue = ledValue;
+}
+  
+void InformationTask::init(int period){
+  Task::init(period);
+}
+  
+void InformationTask::tick(){
+  if (pDoor->isSystemInfo()) {
+    if (map(ledValue->getCurrentIntensity(),0,255,0,100) != 0 && map(ledValue->getCurrentIntensity(),0,255,0,100) != 100) {
+      pDoor->sendToSerial(MSG_INFORMATION + String(temp->readTemperature()) + ":" + String(map(ledValue->getCurrentIntensity(),0,255,0,100)+1));
+    } else {
+      pDoor->sendToSerial(MSG_INFORMATION + String(temp->readTemperature()) + ":" + String(map(ledValue->getCurrentIntensity(),0,255,0,100))); 
+    }
+    pDoor->setSystemInfo(false);
+  }
+}
+
---
+++ smart_door/SerialCommTask.cpp
@@ -0,0 +1,30 @@
+#include "SerialCommTask.h"
+#include "Config.h"
+
+#define MSG_VALID_OK "Valid:T"
+#define MSG_VALID_KO "Valid:F"
+#define MSG_INFORMATION "InfoRequest"
+
+SerialCommTask::SerialCommTask(Door* pDoor){
+  this->pDoor = pDoor;
+}
+  
+void SerialCommTask::init(int period){
+  Task::init(period);
+}
+  
+void SerialCommTask::tick(){
+  if (MsgService.isMsgAvailable()){
+    Msg* msg = MsgService.receiveMsg(); 
+    const String& content = msg->getContent();
+    if (content == MSG_VALID_OK){
+      pDoor->setPermissionOk(true);
+    } else if (content == MSG_VALID_KO){
+      pDoor->setPermissionKo(true);
+    } else if (content == MSG_INFORMATION){
+      pDoor->setSystemInfo(true);
+    }
+    delete msg;    
+  }
+}
+
